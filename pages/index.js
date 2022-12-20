33ca7cfe0d6118a5097fcb4fe8628129d9732a15
Szymon Wilczek <kazikwilczek7@gmail.com>
2022-12-20 23:09:44 +0100

    Dodanie analityk

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import NewsletterForm from '@/components/NewsletterForm'
+import { Analytics } from '@vercel/analytics/react';
 
 const MAX_DISPLAY = 5
 
@@ -95,6 +96,7 @@ export default function Home({ posts }) {
           <NewsletterForm />
         </div>
       )} */}
+      <Analytics />
     </>
   )
 }
